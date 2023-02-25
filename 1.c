0d4a938a00edab8f797731d249c8a9091aa22799
Karthikagarikapati <96178166+Karthikagarikapati@users.noreply.github.com>
2023-02-25 10:34:40 +0530

    Update 1.c

--- 1.c
+++ 1.c
@@ -1,8 +1,6 @@
-#include <stdio.h>
-
-int main()
+public class Main
 {
-    printf("Hello World");
-
-    return 0;
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
 }
